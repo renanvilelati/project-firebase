f0428334fd09b25da581e0604c1ef13786d3d120
Renan Vilela <renanvilelati@gmail.com>
2023-04-08 18:32:45 -0300

    Criando conexão com o app firebase e com o banco firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { db } from './firebaseConnection'
+
 import './App.css'
 
 export const App = () => {
---
+++ src/firebaseConnection.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// Configuração pega no site do firebase para cada app
+const firebaseConfig = {
+    apiKey: "AIzaSyDN3l8Qo4eHpNYOIX4umjxkfNqvyE17Hfc",
+    authDomain: "curso-680af.firebaseapp.com",
+    projectId: "curso-680af",
+    storageBucket: "curso-680af.appspot.com",
+    messagingSenderId: "47313930713",
+    appId: "1:47313930713:web:4138616116834d4e4eb1c8"
+};
+
+// Inicializando o app
+const firebaseApp = initializeApp(firebaseConfig)
+
+// Recuperando o banco firestore
+export const db = getFirestore(firebaseApp)
+
